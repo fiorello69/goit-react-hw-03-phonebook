0cc2b09540edcd352c313e86235f8aa590a3a9a7
Viorel Breban <vbreban69@gmail.com>
2024-01-29 22:01:23 +0100

    pattern modified

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -32,7 +32,7 @@ class ContactForm extends Component {
             name="name"
             value={name}
             onChange={this.handleChange}
-            pattern="/^[a-zA-Z]+( [a-zA-Z]+)+$/"
+            pattern="^[a-zA-Z]+(([' -][a-zA-Z ])?[a-zA-Z]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces."
             required
           />
@@ -45,7 +45,7 @@ class ContactForm extends Component {
             name="number"
             value={number}
             onChange={this.handleChange}
-            pattern="/^[0-9]+$/"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
           />
